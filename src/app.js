7885841f111fb916f04492f6c4fe6430aa66d46d
Tejas <tejastitare30@gmail.com>
2025-10-26 15:48:19 +0530

    Exploring Get, Post, Delete

--- src/app.js
+++ src/app.js
@@ -1,14 +1,26 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.use("/test",(req,res)=>{
-    res.send('Hello from the dashboard!');
+
+app.use("/user",(req,res)=>{
+    res.send("User Created");
 })
-app.use("/",(req,res)=>{
-    res.send('Hello Hello Hell!');
+app.get("/user",(req,res)=>{
+    res.send({name:"Tejas", age:28, city:"New York"});
 })
-app.use("/hello",(req,res)=>{
-    res.send('Hello World');
+
+app.post("/user",(req,res)=>{
+    res.send("User created successfully!");
 })
-app.listen(7777,()=> console.log('Server is running on port 7777'));
+app.use("/test", (req, res) => {
+  res.send("Hello from the Server!");
+});
+
+app.delete("/user",(req,res)=>{
+    res.send("User deleted successfully!");
+})
+// app.use("/", (req, res) => {
+//   res.send("Hello Hello Hell!");
+// });
+app.listen(7777, () => console.log("Server is running on port 7777"));
