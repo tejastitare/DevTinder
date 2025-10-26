764b7feb4892b0435cbd7a47bbd033beff27f55b
Tejas <tejastitare30@gmail.com>
2025-10-26 16:33:53 +0530

    Playing with the Routes

--- src/app.js
+++ src/app.js
@@ -3,12 +3,17 @@ const express = require("express");
 const app = express();
 
 
-app.use("/user",(req,res)=>{
-    res.send("User Created");
-})
+// app.use("/user",(req,res)=>{
+//     res.send("User Created");
+// })
 app.get("/user",(req,res)=>{
+    console.log(req.query);
     res.send({name:"Tejas", age:28, city:"New York"});
 })
+// app.get("/user/:userid/:name/:password",(req,res)=>{
+//     console.log(req.params);
+//     res.send({name:"Tejas", age:28, city:"New York"});
+// })
 
 app.post("/user",(req,res)=>{
     res.send("User created successfully!");
