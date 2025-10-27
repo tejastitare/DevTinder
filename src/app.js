65d451ebb63e9bc9add7cba32c84b12199697f85
Tejas <tejastitare30@gmail.com>
2025-10-27 10:29:54 +0530

    Wild-Card error handling

--- src/app.js
+++ src/app.js
@@ -1,36 +1,20 @@
 const express = require("express");
 
 const app = express();
-const { adminAuth ,userAuth} = require("./middlewares/auth");
-// app.use("/user",e1,[e2,e3],e4,e5,[e6,e7,e8,e9],e10);
 
-app.use("/admin", adminAuth);
+app.get("/getUserData", (req, res, next) => {
+//   try {
+    throw new Error("An error occurred!");
+    res.send("User data Sent");
+//   } catch (err) {
+//     res.status(500).send("Server error");
+//   }
+});
 
-app.get(
-  "/admin/getAllData",
-  (req, res, next) => {
-    res.send("All User Data Sent");
-  },
-  (req, res, next) => {
-    console.log("Handling the route user 3!");
-    // res.send("Hello from the User Page");
-    next();
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Something went wrong!");
   }
-);
 
-app.get("/user",userAuth,(req,res,next)=>{
-    res.send("User data sent successfully");
-})
-
-app.get(
-  "/admin/deleteUser",
-  (req, res, next) => {
-    res.send("User deleted successfully");
-  },
-  (req, res, next) => {
-    console.log("Handling the route user 5!");
-    // next();
-    res.send("Successfully handled the route user 5!");
-  }
-);
+});
 app.listen(7777, () => console.log("Server is running on port 7777"));
