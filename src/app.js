b8fbab0938d56d921acf9254abef389604aacb71
Tejas <tejastitare30@gmail.com>
2025-10-27 19:02:52 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const app = express();
 const User = require("./models/user");
 
 app.use(express.json());
+
 app.post("/signup", async (req, res) => {
     console.log(req.body);
 
@@ -21,6 +22,32 @@ app.post("/signup", async (req, res) => {
     res.send("User registered successfully");
   } catch (err) {
     res.status(400).send("Error registering user: ", +err.message);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const users = await User.findOne({ emailId: userEmail });
+    // const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+// Feed  API -GET /feed - get all the users from the database
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("something went wrong");
   }
 });
 
