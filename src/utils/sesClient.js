30ed6f2a4e53a355400fd6a9927bc744bc1523bb
Tejas <tejastitare30@gmail.com>
2025-11-11 10:19:31 +0530

    secured credentials

---
+++ src/utils/sesClient.js
@@ -0,0 +1,13 @@
+const { SESClient } = require("@aws-sdk/client-ses");
+// Set the AWS Region.
+const REGION = "ap-south-1";
+// Create SES service object.
+const sesClient = new SESClient({ region: REGION, credentials:{
+    accessKeyId:process.env.AWS_ACCESS_KEY,
+    secretAccessKey:process.env.AWS_SECRET_KEY
+} });
+
+console.log("AWS KEY:", process.env.AWS_ACCESS_KEY ? "Loaded ✅" : "Missing ❌");
+
+module.exports = { sesClient };
+// snippet-end:[ses.JavaScript.createclientv3]
